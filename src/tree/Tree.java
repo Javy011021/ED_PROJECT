6d787ee8b42afad7d704df250074657a4f1db879
Javy011021 <garciaj1246@gmail.com>
2022-11-14 23:11:21 -0500

    add tree lib

---
+++ src/tree/Tree.java
@@ -0,0 +1,44 @@
+package tree;
+
+import java.util.List;
+
+abstract public class Tree<E> {
+
+	protected TreeNode<E> root;
+	
+	public Tree() {
+		root = null;
+	}
+
+	public Tree(TreeNode<E> root) {
+		this.root = root;
+	}
+
+	public TreeNode<E> getRoot() {
+		return root;
+	}
+
+	public void setRoot(TreeNode<E> root) {
+		this.root = root;
+	}
+
+	public boolean isEmpty() {
+		return root == null;
+	}
+	
+	public abstract int totalNodes();
+	
+	public abstract List<TreeNode<E>> getLeaves();
+	
+	public abstract int nodeLevel(TreeNode<E> node);
+	
+	public abstract int treeLevel();
+	
+	public abstract int treeHeight();
+	
+	public abstract boolean nodeIsLeaf(TreeNode<E> node);
+	
+	public abstract int nodeDegree(TreeNode<E> node);
+	
+	public abstract E deleteNode(BinaryTreeNode<E> node);	
+} 
