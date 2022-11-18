d0ef16eb6c4ac23d91de56bf8d70babce03e7392
Javy011021 <garciaj1246@gmail.com>
2022-11-18 14:29:57 -0500

    Feat: add method processString

--- src/logic/Huffman.java
+++ src/logic/Huffman.java
@@ -1,5 +1,21 @@
 package logic;
 
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+
 public class Huffman {
-	
+    public PriorityQueue<HuffmanNode> processString (String message){
+        Map<Character, Integer> frequency = new HashMap<>();
+        for (char c: message.toCharArray()){
+            frequency.put(c,frequency.getOrDefault(c,0)+1);
+        }
+        PriorityQueue<HuffmanNode>result = new PriorityQueue<>(Comparator.comparingInt(HuffmanNode::getFrequency));
+        for(Map.Entry<Character, Integer> aux : frequency.entrySet()){
+            result.add(new HuffmanLeaf(aux.getValue(), aux.getKey()));
+        }
+        return result;
+    }
 }
