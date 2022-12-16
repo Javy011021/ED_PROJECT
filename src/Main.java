a71e4156accc908a9ab062b8e432fb3bfdc97445
Javy011021 <garciaj1246@gmail.com>
2022-12-15 23:53:48 -0500

    Feat method sendEmail

--- src/Main.java
+++ src/Main.java
@@ -13,39 +13,6 @@ import javax.mail.internet.MimeMultipart;
 public class Main {
 
     public static void main(String[] args)  {
-        try {
-            Properties props = new Properties();
 
-            props.setProperty("mail.smtp.host", "smtp.gmail.com");
-            props.setProperty("mail.smtp.starttls.enable", "true");
-            props.setProperty("mail.smtp.port","587");
-            props.setProperty("mail.smtp.user", "garciaj1246@gmail.com");
-            props.setProperty("mail.smtp.auth", "true");
-
-            Session session = Session.getDefaultInstance(props);
-
-            BodyPart testroot = new MimeBodyPart();
-            testroot.setText("Texto del mensaje");
-            BodyPart adjunto = new MimeBodyPart();
-            adjunto.setDataHandler(new DataHandler(new FileDataSource("correo.txt")));
-            adjunto.setFileName("correo.txt");
-            MimeMultipart multiParte = new MimeMultipart();
-            multiParte.addBodyPart(testroot);
-            multiParte.addBodyPart(adjunto);
-
-            MimeMessage message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("garciaj1246@gmail.com"));
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress("richardoro2001@gmail.com"));
-            message.setSubject("Hola");
-            message.setContent(multiParte);
-
-            Transport t = session.getTransport("smtp");
-            t.connect("garciaj1246@gmail.com", "wrfcocekpkcxhqvq");
-            t.sendMessage(message, message.getAllRecipients());
-            t.close();
-
-        }catch (Exception e){
-            e.printStackTrace();
-            }
     }
 }
